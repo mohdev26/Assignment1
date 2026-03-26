4793b3ed77e2ab0629fc1d231da423131aae688b
Mohamed Abdulgayed <moh.dev26@gmail.com>
2026-03-26 23:53:06 +0200

    update bonus.js

--- bonus.js
+++ bonus.js
@@ -1,7 +1,18 @@
-let count = 0;
-let candidate;
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var majorityElement = function(nums) {
+    let candidate = null;
+    let count = 0;
 
-for (let num of nums) {
-    if (count === 0) candidate = num;
-    count += (num === candidate) ? 1 : -1;
-}
+    for (let num of nums) {
+        if (count === 0) {
+            candidate = num;
+        }
+
+        count += (num === candidate) ? 1 : -1;
+    }
+
+    return candidate;
+};
